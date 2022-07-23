a0809e820f149bf2219ab2d799e654e258f64197
fidel <fidelisvictor18@gmail.com>
2022-07-23 13:06:17 -0300

    Antecessor e Sucessor

--- exe005.py
+++ exe005.py
@@ -1,5 +1,6 @@
 # Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e antecessor.
 n = int(input('Digite um número: '))
-print('Analisando o valor {}, seu sucessor é {} e seu antecessor é {}'.format(n, (n+1), (n-1)))
+print(f'Analisando o valor {n}, seu sucessor é {n+1} e seu antecessor é {n-1}')
+
 
 
