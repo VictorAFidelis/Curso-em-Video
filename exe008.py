69a4e9660ce9585aa2a0b91ed0951f1d6f2fc1af
fidel <fidelisvictor18@gmail.com>
2022-07-23 13:14:02 -0300

    Conversor de Medidas em cm e mm

--- exe008.py
+++ exe008.py
@@ -1,6 +1,5 @@
 # Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros.
-
 n = int(input('Digite uma metragem '))
-print('{} metros, correspondem a: \n{} centimetros e \n{} milimetros' .format((n), (n*100), (n*1000)))
+print(f'{n} metros, correspondem a: \n{n*100} centimetros e \n{n*1000} milimetros')
 
 
