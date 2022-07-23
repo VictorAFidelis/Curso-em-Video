9ccda2b1ba72d18360110125feeaa33f011ddb52
fidel <fidelisvictor18@gmail.com>
2022-07-23 13:07:59 -0300

    Dobro, Triplo e Raiz Quadrada

--- exe06.py
+++ exe06.py
@@ -1,7 +1,7 @@
 # Crie um algoritmo que leia um número e mostre o seu dobro, triplo e a raiz quadrada.
-
 n = int(input('Digite um número '))
-print('O dobro é {} \nO trilo é {} \nE a raiz quadrada é {} '.format((n*2), (n*3), (n**(1/2))))
+print(f'O dobro é {n*2} \nO trilo é {n*3} \nE a raiz quadrada é {n**(1/2)} ')
+
 
 
 
