c524693a268bea2677646cb58cb94952b909143d
fidel <fidelisvictor18@gmail.com>
2022-07-24 22:07:53 -0300

    Analisador de Textos

--- exe022.py
+++ exe022.py
@@ -2,15 +2,20 @@
 # - O nome com todas as letras maiúsculas e minúsculas.
 # - Quantas letras ao t odo.
 # - Quantas letras tem o primeiro nome.
-
-nome = str(input('Digite seu nome compeleto:' )).strip()
+from time import sleep
+nome = str(input('Digite seu nome completo: ')).strip()
 print('Analisando seu nome...')
-print('Seu nome em letras maiúsculas é: {}'.format(nome.upper()))
-print('Seu nome em letras minusculas é: {}'.format(nome.lower()))
-print('Seu nome tem {} letras'.format(len(nome) - nome.count(' ')))
-#print('Seu primeiro nome tem {} letras '.format(nome.find(' ')))
+sleep(3)
+print(f'Seu nome em letras maiúsculas é: {nome.upper()}')
+sleep(1)
+print(f'Seu nome em letras minusculas é: {nome.lower()}')
+sleep(1)
+print(f'Seu nome tem {len(nome)} letras')
+sleep(1)
 separa = nome.split()
-print('Seu primeiro nome é {} e ele tem {} letras'.format(separa[0], len(separa[0])))
+print(f'Seu primeiro nome é {separa[0]} e ele tem {len(separa[0])} letras')
+
+
 
 
 
