dca659953a3acc889e99a103a761ec090542996e
fidel <fidelisvictor18@gmail.com>
2022-07-24 21:50:45 -0300

    Calculando Descontos

--- exe012.py
+++ exe012.py
@@ -1,8 +1,7 @@
 # Faça um algoritmo que leia o preço de um produto e mostre seu novo preço com desconto de 5%..
+produto = float(input('Qual o preço do produto: R$ '))
+desconto = produto - (produto * 5 / 100)
+print(f'O produto que custava R${produto:.2f}, na promoção com desconto de 5% vai custar R${desconto:.2f}.')
 
-prod = float(input('Qual o preço do produto: R$ '))
-des = prod - (prod * 5 / 100)
-
-print('O produto que custava R${:.2f}, na promoção com desconto de 5% vai custar R${:.2f}.'. format(prod, des))
 
 
