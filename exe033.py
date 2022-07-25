f11d9ef24184df873e43fd30b667f2a5713eb009
fidel <fidelisvictor18@gmail.com>
2022-07-24 22:33:17 -0300

    Maior e Menor valores

--- exe033.py
+++ exe033.py
@@ -1,5 +1,4 @@
 # Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
-
 n1 = int(input('Digite o primeiro número: '))
 n2 = int(input('Digite o segundo número: '))
 n3 = int(input('Digite o terceiro número: '))
@@ -15,5 +14,6 @@ if n2>n1 and n2>n3:
     maior = n2
 if n3>n1 and n3>n2:
     maior = n3
-print('Dos números digitados o maior número é {} e o menor número é {}'. format(maior, menor))
+print(f'Dos números digitados o maior número é {maior} e o menor número é {menor}')
+
 
