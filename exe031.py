2ab6bfe588178c08cb09a6250a2d33adef1e3a74
fidel <fidelisvictor18@gmail.com>
2022-07-24 22:31:34 -0300

    Custo da Viagem

--- exe031.py
+++ exe031.py
@@ -1,13 +1,13 @@
 # Desenvolva um programa que pergunte a distância de uma viagem em Km.
 # Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 parta viagens mais longas.
-
 v = float(input('Qual a distancia da sua viagem? '))
 if v <= 200:
-    pas = v * 0.50
-    print('O valor da passagem será de R$ {:.2f}'.format(pas))
+    passagem_curta = v * 0.50
+    print(f'O valor da passagem será de R$ {passagem_curta:.2f}')
 else:
-    pa = v * 0.45
-    print('O valor da passagem será R$ {:.2f}'.format(pa))
+    passagem_longa = v * 0.45
+    print(f'O valor da passagem será R$ {passagem_longa:.2f}')
+
 
 
 
