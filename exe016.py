da6c9ab3f7819436348d474775e1cb53800630ed
fidel <fidelisvictor18@gmail.com>
2022-07-24 21:56:38 -0300

    Quebrando um numero

--- exe016.py
+++ exe016.py
@@ -1,7 +1,7 @@
 # Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
 import math
-
 num = float(input('Digite um numero: '))
 real = math.trunc(num)
-print('O valor digitado foi {} e sua porção inteira é {}'.format(num, math.trunc(num)))
+print(f'O valor digitado foi {num} e sua porção inteira é {math.trunc(num)}')
+
 
