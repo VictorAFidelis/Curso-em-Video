915d3f06c6cf6fc0cb6f8c91a8309b191a10ab6d
fidel <fidelisvictor18@gmail.com>
2022-07-24 21:48:16 -0300

    Conversor de Moeda

--- exe010.py
+++ exe010.py
@@ -1,9 +1,8 @@
 # Crie um programa que leia quanto dinheiro uma pessoa tenha na carteira e mostre quantos Dólares ela pode comprar.
 # Considera U$$ 1,00 = R$ 3,27
-
-real = float(input(' Qual valor você tem na carteira: R$'))
+real = float(input('Qual valor você tem na carteira: R$ '))
 dolar = real / 5.44
 euro = real / 6.13
-print('Com R${:.2f} você pode comprar \nUS${:.2f} e \nE${:.2f}'.format(real, dolar, euro))
+print(f'Com R${real:.2f} você pode comprar \nUS${dolar:.2f} e \nE${euro:.2f}')
 
 
