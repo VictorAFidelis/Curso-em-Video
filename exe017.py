fa3a48ed3d66a353c9418ae1f82653d3780dc25f
fidel <fidelisvictor18@gmail.com>
2022-07-24 21:59:18 -0300

    Catetos e Hipotenusa

--- exe017.py
+++ exe017.py
@@ -5,10 +5,10 @@ co = float(input('Digite o valor do cateto oposto: '))
 ca = float(input('Digite o valor do cateto adjacente: '))
 hi = (co **2 + ca **2) ** (1/2)
 print('O valor da hipotenusa é {}'.format(hi)) '''
-
 import math
-
 co = float(input('Digite o valor do cateto oposto: '))
 ca = float(input('Digite o valor do cateto adjacente: '))
 hi = math.hypot(ca, co)
-print('O valor da hipotenusa é {:.2f}'.format(hi))
+print(f'O valor da hipotenusa é {hi:.2f}')
+
+
