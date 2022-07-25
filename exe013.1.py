ac9a678d4af37f9fe2e0ca927ca6003d01e52ee1
fidel <fidelisvictor18@gmail.com>
2022-07-24 21:52:19 -0300

    Calculando Descontos a vista ou a prazo

--- exe013.1.py
+++ exe013.1.py
@@ -1,6 +1,5 @@
 # Um produto de tem um valor para venda. Se o pagamento for a vista recebe 5% de deconto. A prazo terá 8% de acréscimo.
-
-prod = float(input('Qual o valor do produto: R$ '))
-vista = prod - (prod * 5 / 100)
-prazo = prod + (prod * 8 / 100)
-print('As formas de pagamento para esse produto são:\nA vista {:.2f} \nPrazo {:.2f}'.format(vista, prazo))
+produto = float(input('Qual o valor do produto: R$ '))
+vista = produto - (produto * 5 / 100)
+prazo = produto + (produto * 8 / 100)
+print(f'As formas de pagamento para esse produto são:\nA vista {vista:.2f} \nPrazo {prazo:.2f}')
