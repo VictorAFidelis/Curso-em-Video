c1e7b4edee765849066c6a1b04097c4e0ad19ab7
fidel <fidelisvictor18@gmail.com>
2022-07-24 22:26:53 -0300

    Primeiro e ultimo nome de uma pessoa

--- exe027.py
+++ exe027.py
@@ -1,10 +1,10 @@
 # aça um programa que leia o nome completo de uma pessoa,
 # mostrando em seguida o primeiro e o último nome separadamente.
-
 nome = str(input('Digite seu nome completo:')).strip()
 dividido = nome.split()
-print('Seu primeiro nome é {}'.format(dividido[0]))
-print('Seu último nome é {}'.format(dividido[len(dividido)-1]))
+print(f'Seu primeiro nome é {dividido[0]}')
+print(f'Seu último nome é {dividido[len(dividido)-1]}')
+
 
 
 
