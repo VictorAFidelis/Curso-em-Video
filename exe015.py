8f93458d6855e4fadb09738afe9952eb218c12d1
fidel <fidelisvictor18@gmail.com>
2022-07-24 21:55:37 -0300

    Aluguel de Carros

--- exe015.py
+++ exe015.py
@@ -1,13 +1,13 @@
 # Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias
 # pelos quais ele foi alugado.
 # Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
-
 dias = int(input('Por quantos dias o carro foi alugado: '))
 km = float(input('Quantos km foram percorridos com esse carro: '))
 diaria = dias * 60
 kms = km * 0.15
 total = diaria + kms
-print('Seus custos são: \nAluguel: R${} \nKilometragem: R${:.2f} \nTotal: {:.2f}'.format(diaria, kms, total))
+print(f'Seus custos são: \nAluguel: R${diaria} \nKilometragem: R${kms:.2f} \nTotal: {total:.2f}')
+
 
 
 
