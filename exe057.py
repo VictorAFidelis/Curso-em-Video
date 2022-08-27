6db025265cd96f34ba8a8a2d6bcfd83654200478
fidel <fidelisvictor18@gmail.com>
2022-08-26 22:39:14 -0300

    Validação de Dados

--- exe057.py
+++ exe057.py
@@ -5,10 +5,3 @@ sexo = str(input('Digite o seu sexo. [M/F]:')).strip().upper()[0]
 while sexo not in 'MF':
     sexo = str(input('Dados invalidos. Por favor infome seu sexo: ')).strip().upper()[0]
 print('Sexo {} registrado com sucesso.'.format(sexo))
-
-
-
-
-
-
-
